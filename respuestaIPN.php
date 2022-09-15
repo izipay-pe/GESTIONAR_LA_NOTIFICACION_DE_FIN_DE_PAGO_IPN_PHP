ac261f00f963b40de5f407111a525b79e87fa1f9
izipay-pe <soporteecommerce@izipay.pe>
2022-09-15 16:30:45 -0500

    update readme

--- respuestaIPN.php
+++ respuestaIPN.php
@@ -1,2 +1,5 @@
 <?php
- file_put_contents("respuestasIPN/notificacionIPN-".time(),json_encode($_POST));
+// IPN del formulario API V1, V2 y API REST respectvamente"
+if( isset($_POST["vads_hash"]) || isset($_POST["kr-hash"]) ){
+    file_put_contents("respuestasIPN/notificacionIPN-".time(),json_encode($_POST));
+}
